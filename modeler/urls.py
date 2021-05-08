dff600435d8120bb9cd69e90f5edce18cd1abc44
austiller <github@austins.site>
2021-05-08 13:25:42 -0500

    bug fixes

--- modeler/urls.py
+++ modeler/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('<int:pk>/delete/',views.ModelDeleteView.as_view(),name='delete_model'),
     path('<int:pk>/start/',views.startModel, name='start_certstream_task'),
     path('<int:pk>/stop/',views.stopModel, name='stopModel'),
-    path('download/All/',views.csvAll,name='download_all'),
-    path('download/Malicious/',views.csvMalicious,name='download_malicious')
+    path('download/All/',views.csv_all,name='download_all'),
+    path('download/Malicious/',views.csv_malicious,name='download_malicious')
 
 ]
